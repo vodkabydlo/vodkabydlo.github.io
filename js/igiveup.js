d031ace0decd7b32cb910025648ea2c188d35fab
irisrumtub <tvburis@gmail.com>
2022-08-17 17:41:45 +0300

    ne rabotaet

--- js/igiveup.js
+++
@@ -1,6 +0,0 @@
-function open_bitch() {
-document.getElementById("fold-out-menu").style.transform="none"
-}
-function close_bitch() {
-    document.getElementById("fold-out-menu").style="transform-origin: 0% 0%; transform: translate(-100%, 0); transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);"
-    }
